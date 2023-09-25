637860f747e294f17c1bce3c08ea3ab22183d7b2
mateus <mateusczdev@gmail.com>
2023-09-25 11:17:42 -0300

    update javascript

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ function toggleMode() {
   const img = document.querySelector(".profile img")
 
   if (html.classList.contains("light")) {
+    img.setAttribute("alt", "Background modo light.")
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Background modo dark.")
   }
 }
